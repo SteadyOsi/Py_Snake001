d9a483f4347c2abe8002157550f2e2495de42853
SteadyOsi <Josiahstedman@gmail.com>
2025-09-23 12:05:56 +1000

    nothing

--- Simple-snake.py
+++ Simple-snake.py
@@ -97,6 +97,6 @@ while running:
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(2)  # limits FPS to 60
+    clock.tick(10)  # limits FPS to 60
 
 pygame.quit()
